be54f5a677a53fa140dcbe5b515804dc5500567a
Plean Somjit <plean_som@cmu.ac.th>
2025-10-16 06:41:13 +0700

    create file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module se233._331lab09auctionback {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens se233._331lab09auctionback to javafx.fxml;
+    exports se233._331lab09auctionback;
+}
